30ab050cea1a8db1a1c6a68e36f10accf830d0f8
1102-cosgrove-addy <addycosgrove@nevada.unr.edu>
2022-01-28 21:19:12 -0800

    Done

---
+++ variablepgrm.c
@@ -0,0 +1,16 @@
+int main ()
+{
+ //Initializes and sets values for variables
+char gradeOne = 'C';
+char gradeTwo = 'C';
+char grade Three ='C';
+
+//Prints the values of the variables
+printf("Grade 1: %c\n",gradeOne);
+printf("Grade 2: %c\n", gradeTwo);
+printf("Grade 3: %c\n", gradeThree);
+
+//Exits the program
+return 0;
+
+}
